84778c05d98b1090114c909d1a6412dc9fe92763
Divyansh Gupta <104493538+divyansh980@users.noreply.github.com>
2025-06-10 08:25:16 +0530

    Update db.php

--- ajax/db.php
+++ ajax/db.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "shas1125_easypadhai";
+    $database = "study_sphere_db";
     
     $conn = new mysqli($servername, $username, $password, $database);
     
